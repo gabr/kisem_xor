08effdf8ab33c500857c7d5b50485293e9d17cb4
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2015-01-14 06:45:55 +0100

    Smaller training data input window

--- XOR/XORForm.Designer.cs
+++ XOR/XORForm.Designer.cs
@@ -471,11 +471,11 @@
             // textBox_trainData
             // 
             this.textBox_trainData.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox_trainData.Location = new System.Drawing.Point(669, 386);
+            this.textBox_trainData.Location = new System.Drawing.Point(748, 386);
             this.textBox_trainData.Multiline = true;
             this.textBox_trainData.Name = "textBox_trainData";
             this.textBox_trainData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox_trainData.Size = new System.Drawing.Size(205, 109);
+            this.textBox_trainData.Size = new System.Drawing.Size(126, 109);
             this.textBox_trainData.TabIndex = 44;
             // 
             // XORForm
